f01015eb0b1eb192cdfef38a9f7e34b46fa26ce1
Andrey Bobr <a.bobr@itransition.com>
2021-05-19 14:49:42 +0300

    Added localization files, added swiftformat script

--- ADMVEDU/Network/NetworkError.swift
+++ ADMVEDU/Network/NetworkError.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 enum NetworkError: Error {
-	var localized: String {
-		return ErrorLocalization.networkErrorLocalization(error: self)
-	}
-
 	case notFound
 	case invalidData
 	case unexpected(code: Int)
@@ -21,7 +17,18 @@ extension NetworkError {
 }
 
 extension NetworkError: LocalizedError {
-	public var errorDescription: String? {
-		return ErrorLocalization.networkErrorLocalization(error: self)
+	var localized: String? {
+		switch self {
+		case .invalidData:
+			return localizedError(error: "The provided data is not valid.", comment: "")
+		case .notFound:
+			return localizedError(error: "The specified item could not be found.", comment: "Resource Not Found")
+		case .unexpected:
+			return localizedError(error: "An unexpected error occurred.", comment: "Unexpected Error")
+		}
+	}
+
+	private func localizedError(error: String, comment: String) -> String {
+		return NSLocalizedString(error, comment: comment)
 	}
 }
